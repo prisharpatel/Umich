65df5bb5bf3143cc462166d27184fb02640f7d16
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-10-17 12:49:18 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,13 +1,34 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums = set(nums)
-        i = 1
-        maxVal = max(nums)
-        while i in nums and i < len(nums)+1:
-            i += 1
-        if i == len(nums):
-            return len(nums) 
-        return i 
+        # nums = set(nums)
+        # i = 1
+        # maxVal = max(nums)
+        # while i in nums and i < len(nums)+1:
+        #     i += 1
+        # if i == len(nums):
+        #     return len(nums) 
+        # return i 
+
+        # use nums as a hashset of proviing if a number exists in array by turning it to a negative value if 
+        # value exists at i - 1 index (EX: 1 exists if nums[0] is negative)
+
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                nums[i] = 0
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            # edit the array to a negative number at pos i - 1 if it isn't already 
+            if 1 <= val <= len(nums):
+                if nums[val-1] > 0:
+                    nums[val-1] *= -1
+                elif nums[val-1] == 0:
+                    nums[val-1] = len(nums) + 1
+        for i in range(1, len(nums) + 1):
+            if nums[i-1] >= 0:
+                return i 
+        return len(nums) + 1
+
+       
 
         
 
