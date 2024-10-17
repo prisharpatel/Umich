99bb445d4328c0deab3305fdc1be70e9df0f62cb
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-10-17 12:19:42 -0700

    Time: 29 ms (100%), Space: 33.8 MB (25.81%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -3,10 +3,10 @@ class Solution:
         nums = set(nums)
         i = 1
         maxVal = max(nums)
-        while i in nums and i < maxVal:
+        while i in nums and i < len(nums)+1:
             i += 1
-        if i == maxVal:
-            return maxVal + 1 
+        if i == len(nums):
+            return len(nums) 
         return i 
 
         
