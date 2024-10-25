318edc4072822513078016dd5b739961e0133458
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-10-25 17:25:11 -0400

    Time: 373 ms (52.77%), Space: 70.4 MB (24.81%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.py
@@ -5,7 +5,7 @@ def delete_duplicate_emails(person: pd.DataFrame) -> None:
     # delete all duplicate emails - keep one with smallest id 
     # sort by emails and then sort by ids and then do drop duplicates with keep first and delete rest
     person.sort_values(by="id", inplace=True)
-    print(person)
+    # print(person)
     person.drop_duplicates(subset="email", inplace=True)
     
     # return person
