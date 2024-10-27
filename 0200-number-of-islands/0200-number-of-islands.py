aa8f93a8d5d907e97caa5b50f377e60590a8c97c
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-10-27 12:40:43 -0400

    Time: 258 ms (38.88%), Space: 27.9 MB (7.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,39 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # 1 = land
+        # 0 = water 
+        rows = len(grid)
+        cols = len(grid[0])
+        # return the number of islands 
+
+        # matching - dfs
+
+        # psuedocode 
+        visited = set() # set of tuples for ordered pairs 
+        islandSize = 0
+        stack = deque() # pop left() and appendleft() - add tuples of places
         count = 0
-        # U = return the number of groups of 1
-        # M - DFS --> stack 
-        # P - find a 1 and then search everywhere around it 
-            # check if the current spot is valid to check!
-            # if its a 1 --> conver to 0 and then search for more island (search everywhere around it and search everyhwere around those values --> DFS because going further and further into this path )
-            # if its a 0 --> end this DFS search
-        def dfs(grid, r, c):
-            if (r < 0 or c<0 or r >= len(grid) or c >= len(grid[0])):
-                # this spot is out of bounds so don't need to check anything
-                return 
-            if (grid[r][c] != "1"):
-                # we reached water don't follow this path bc nowhere to go or already visited
-                return 
-            # turn this spot to -1 to say we already visited this node
-            grid[r][c] = "-1"
-            dfs(grid, r-1, c)
-            dfs(grid, r, c+1)
-            dfs(grid, r+1, c)
-            dfs(grid, r, c-1)
+
+        # see what triggers dfs and add that many to count 
+
         
-        count  = 0 # counts the number of islands
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == "1":
+        def dfs(i, j): 
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] != "1" or (i, j) in visited:
+                return 
+            visited.add((i, j))
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+            
+        for i in range(rows):
+            for j in range(cols):
+                if (i, j) not in visited and grid[i][j] == "1":
+                    dfs(i, j)
                     count += 1
-                    dfs(grid, i, j)
+                    
+        
+
         return count
+
+        
