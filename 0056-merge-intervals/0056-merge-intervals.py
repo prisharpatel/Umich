14bda04bbd252899395ddeda853c41343ffe4285
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-10-28 12:11:16 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,36 +1,30 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # merge intervals based on which ones will be within each other
-        # sort based on the first element of each of the elements in intervals and merge if 
-        # the next one you want to merge with is within it or to the right of it on number line, else add it 
-        # to output 
+        # intervals
+        # intervals[i] = [start, end]
 
-        # sort with lambda
-        intervals = sorted(intervals, key=lambda x: x[0])
-        
-        # for loop to go through intervals and check conditions above 
+        # return array of non-overlapping intervals 
+
+        # matching - sorting, array
+        if intervals is None:
+            return intervals
+
+        # pseduo 1) sort array by the start element
+        intervals = sorted(intervals, key = lambda x: x[0])
+        # have output array 
         output = []
-        current = []
-        for interval in intervals: 
-            if len(output) == 0:
+        # go through sorted array and extend what is in the output array if following conditions are met
+            # adding start < end in array 
+        # else: 
+            # add something new to output 
+        for interval in intervals:
+            if output == []: 
                 output.append(interval)
-                current = interval
+                continue
+            if output[-1][1] >= interval[0]:
+                # extend output 
+                output[-1][1] = interval[1]
             else:
-                # check if you can merge
-                # within 
-                if interval[0] >= current[0] and interval[1] <= current[1]:
-                    continue # don't need to add to output since within another interval already
-                # to teh right
-                if interval[0] >= current[0] and interval[1] >= current[1] and interval[0] <= current[1]:
-                    output.remove(current)
-                    current = [current[0], interval[1]]
-                    output.append(current)
-                    continue
-                # nothing 
-                output.append(interval)
-                current = interval
+                output.append(interval)             
         return output
-                
-
-    
-        
+        # 
