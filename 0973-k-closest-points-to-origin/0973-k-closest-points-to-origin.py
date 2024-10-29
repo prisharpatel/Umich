c83ba97d89941fb3f08a20feccace5acf9ac4a97
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-10-29 09:11:12 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -0,0 +1,32 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # return k closest points to the origin based on euclidean distance which means we 
+        # want k minimum distances 
+        # which is max heap of k distances
+        # EX: 
+        # distances are 1, 2, 5
+        # we find one that is 4 
+
+        # distances are -5, -2, -1
+        # we find one that is -4 which is smaller than largest distnac e
+        # -4 -2 -1 
+        heap = []
+        for i, point in enumerate(points): 
+            x = point[0]
+            y = point[1]
+            distance = sqrt(x**2 + y**2)
+
+            if len(heap) == k and heap[0][0] * -1 > distance:
+                heapq.heappop(heap)
+                
+            heapq.heappush(heap, (distance * -1, i))
+
+        # O(K)
+        output = []
+        for dist, point in heap: 
+            output.append(points[point])
+
+        return output
+
+        
+        
