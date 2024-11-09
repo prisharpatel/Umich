7479205e260cda3483555a3c716fd903624c8031
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-11-09 15:33:20 -0500

    Time: 13 ms (33.8%), Space: 30.7 MB (79.81%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # return true if element appearing in list 
+        # create set - check if set as long as nums (o(N))
+        # have to have it be at lesat O(N) because otherwise you can't go through all elements 
+        # ==> can't optimize 
+        numsSet = set(nums)
+        return len(numsSet) != len(nums) 
+        
