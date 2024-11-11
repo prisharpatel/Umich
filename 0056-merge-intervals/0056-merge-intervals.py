47aa13927d6778796c1de41f6ae9217b80ff6c1f
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-11-10 21:25:09 -0500

    Time: 7 ms (73.87%), Space: 20.1 MB (79.82%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,30 +1,28 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # intervals
-        # intervals[i] = [start, end]
+        # sort by the first time 
+        # check two things 
+            # is the interval within the current interval we are looking at
+            # can the interval extend the current interval we are looking at
 
-        # return array of non-overlapping intervals 
-
-        # matching - sorting, array
-        if intervals is None:
-            return intervals
-
-        # pseduo 1) sort array by the start element
-        intervals = sorted(intervals, key = lambda x: x[0])
-        # have output array 
-        output = []
-        # go through sorted array and extend what is in the output array if following conditions are met
-            # adding start < end in array 
-        # else: 
-            # add something new to output 
+        # can sort in place
+        intervals.sort(key = lambda x: x[0])
+        current = intervals[0] # guaranteed to have length of 1
+        # go through itnervals and check above two checks 
+        merged = []
         for interval in intervals:
-            if output == []: 
-                output.append(interval)
-                continue
-            if output[-1][1] >= interval[0]:
-                # extend output 
-                output[-1][1] = interval[1]
-            else:
-                output.append(interval)             
-        return output
-        # 
+            left = interval[0]
+            right = interval[1]
+            if left >= current[0] and right <= current[1]: 
+                # fits inside current interval, so move on
+                continue 
+            elif  current[0] <= left <= current[1] and right > current[1]: 
+                current[1] = right
+                continue 
+                # extends interval
+            else: 
+                merged.append(current)
+                current = interval
+        merged.append(current)
+        return merged
+        
