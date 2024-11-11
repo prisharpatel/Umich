50c7c7822007c98cef70a8ca82ac2d7c6738d118
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-11-10 21:27:50 -0500

    Time: 0 ms (100%), Space: 20 MB (79.82%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,28 +1,32 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # sort by the first time 
-        # check two things 
-            # is the interval within the current interval we are looking at
-            # can the interval extend the current interval we are looking at
+        # intervals
+        # intervals[i] = [start, end]
 
-        # can sort in place
-        intervals.sort(key = lambda x: x[0])
-        current = intervals[0] # guaranteed to have length of 1
-        # go through itnervals and check above two checks 
-        merged = []
+        # return array of non-overlapping intervals 
+
+        # matching - sorting, array
+        if intervals is None:
+            return intervals
+
+        # pseduo 1) sort array by the start element
+        intervals = sorted(intervals, key = lambda x: x[0])
+        # have output array 
+        output = []
+        # go through sorted array and extend what is in the output array if following conditions are met
+            # adding start < end in array 
+        # else: 
+            # add something new to output 
         for interval in intervals:
-            left = interval[0]
-            right = interval[1]
-            if left >= current[0] and right <= current[1]: 
-                # fits inside current interval, so move on
-                continue 
-            elif  current[0] <= left <= current[1] and right > current[1]: 
-                current[1] = right
-                continue 
-                # extends interval
-            else: 
-                merged.append(current)
-                current = interval
-        merged.append(current)
-        return merged
-        
+            if output == []: 
+                output.append(interval)
+                continue
+            if output[-1][1] >= interval[0] and interval[1] > output[-1][1]:
+                # extend output 
+                output[-1][1] = interval[1]
+            if interval[0] >= output[-1][0] and interval[1] <= output[-1][1]:
+                continue # don't do anything because adding interval is within the one that exists already...
+            else:
+                output.append(interval)             
+        return output
+        # 
