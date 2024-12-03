850e663f088e91226101891394d442188845bc01
Prisha Patel <129897344+prisharpatel@users.noreply.github.com>
2024-12-03 11:15:35 -0500

    Time: 2847 ms (5.02%), Space: 77.7 MB (50.48%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,39 +1,37 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
-        self.capacity = capacity
-        self.lru = deque() # most recently used on right side, so pop from left to get LRU
+        self.cache = {} # dictionary that stores key --> value 
+        self.limit = capacity
+        self.LRU = [] # LRU is at index 0 of this
+        
 
     def get(self, key: int) -> int:
-        # use .remove() to remove and add to back of lru 
-        
         if key in self.cache:
-            self.lru.remove(key)
-            self.lru.append(key)
+            # update LRU 
+            if key in self.LRU:
+                self.LRU.remove(key)
+            self.LRU.append(key) 
             return self.cache[key]
         else:
             return -1
-        
 
     def put(self, key: int, value: int) -> None:
-        # to track least recently 
-        if key in self.cache:
-            self.cache[key] = value
-
-        if len(self.cache.keys()) == self.capacity and key not in self.cache:
-            # remove lru 
-            val = self.lru.popleft() 
-            self.cache.pop(val, None)
-        elif key in self.cache:
-            self.lru.remove(key)
-        # add new key
-        self.cache[key] = value 
-        # add to LRU
-        self.lru.append(key)
-            
+        #  if cache size = to limit
+            # pop off index 0 from LRU and remove that value/key pair from cache 
+        # add new key/value pair to cache 
+        # FOLLOW UP: CAN THE SAME KEY BE INPUTTED TWO TIMES TO THE CACHE? 
+        # check if key in cache already 
         
-
+        if len(self.cache) == self.limit and key not in self.cache: 
+            lru = self.LRU.pop(0) 
+            # how can we gaurantee that lru is in the cache?
+            # only add to lru if it is put into the cache and remove now 
+            self.cache.pop(lru)
+        self.cache[key] = value
+        if key in self.LRU: 
+            self.LRU.remove(key)
+        self.LRU.append(key)
 
         
 
